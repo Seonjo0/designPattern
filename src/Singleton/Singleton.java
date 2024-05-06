ba3e35e6198a5ccc6b84a97f199cef0b6eb202f0
Seon Joo Yi <seonjoo@Seonui-MacBookAir.local>
2024-05-06 21:51:22 +0900

    # 커밋 4개가 섞인 결과입니다. # 1번째 커밋 메시지입니다:
    Singleton Pattern
    
    # 커밋 메시지 #2번입니다:
    
    #Make Better Singleton Pattern
    
    # 커밋 메시지 #3번입니다:
    
    #Make Better Singleton Pattern
    
    # 커밋 메시지 #4번입니다:
    
    #Make Better Singleton Pattern

---
+++ src/Singleton/Singleton.java
@@ -0,0 +1,36 @@
+package Singleton;
+
+public class Singleton {
+//    private static Singleton singletonInstance;
+
+//    public static synchronized Singleton getSingletonInstance() {
+//        if (singletonInstance == null) {
+//            singletonInstance = new Singleton();
+//        }
+//        return singletonInstance;
+//    }
+
+//    private static final Singleton singletonInstance = new Singleton();
+//
+//    public static synchronized Singleton getSingletonInstance() {
+//        return singletonInstance;
+//    }
+
+    private volatile static Singleton singletonInstance;
+
+    public static Singleton getSingletonInstance() {
+        if (singletonInstance == null) {
+            synchronized (Singleton.class) {
+                if (singletonInstance == null) {
+                    //왜 singletoneInstance 를 두 번 체킹하는가?
+                    singletonInstance = new Singleton();
+                }
+            }
+        }
+        return singletonInstance;
+    }
+
+    public void tell() {
+        System.out.println("나는 싱글톤이라네");
+    }
+}
