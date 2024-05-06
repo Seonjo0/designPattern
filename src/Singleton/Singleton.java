84402113d00fd922b95937073a643bccf25a92e7
seonjo0 <goatjswn@oncosoft.io>
2024-05-06 22:04:28 +0900

    Make Better Singleton Pattern

--- src/Singleton/Singleton.java
+++ src/Singleton/Singleton.java
@@ -1,11 +1,31 @@
 package Singleton;
 
 public class Singleton {
+//    private static Singleton singletonInstance;
+
+//    public static synchronized Singleton getSingletonInstance() {
+//        if (singletonInstance == null) {
+//            singletonInstance = new Singleton();
+//        }
+//        return singletonInstance;
+//    }
+
+//    private static final Singleton singletonInstance = new Singleton();
+//
+//    public static synchronized Singleton getSingletonInstance() {
+//        return singletonInstance;
+//    }
+
     private static Singleton singletonInstance;
 
-    public static synchronized Singleton getSingletonInstance() {
+    private static Singleton getSingletonInstance() {
         if (singletonInstance == null) {
-            singletonInstance = new Singleton();
+            synchronized (Singleton.class) {
+                if (singletonInstance == null) {
+                    //왜 singletoneInstance 를 두 번 체킹하는가?
+                    singletonInstance = new Singleton();
+                }
+            }
         }
         return singletonInstance;
     }
